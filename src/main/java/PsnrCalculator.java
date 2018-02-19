0009b286a7dcb793400d55c72b67f0cbaed68561
Felix Limanta <13515065@std.stei.itb.ac.id>
2018-02-19 16:03:30 +0700

    Fixed PSNR calculation, added Vigenere cipher

--- src/main/java/PsnrCalculator.java
+++ src/main/java/PsnrCalculator.java
@@ -2,14 +2,21 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
 public class PsnrCalculator {
+
   public static double calculatePSNR(BufferedImage i1, BufferedImage i2) {
+    double rms = calculateRMS(i1, i2);
+    return 20 * Math.log10(256 / rms);
+  }
+
+  private static double calculateRMS(BufferedImage i1, BufferedImage i2) {
     long s = 0;
 
     byte[] data1 = ((DataBufferByte) i1.getRaster().getDataBuffer()).getData();
     byte[] data2 = ((DataBufferByte) i2.getRaster().getDataBuffer()).getData();
 
-    if (data1.length != data2.length)
+    if (data1.length != data2.length) {
       throw new IllegalArgumentException("Different image size");
+    }
 
     for (int i = 0; i < data1.length; ++i) {
       int d = (data1[i] & 0xff) - (data2[i] & 0xff);
---
+++ src/main/java/VigenereCipher.java
@@ -0,0 +1,39 @@
+public class VigenereCipher {
+
+  private final static Operation encipherOp =
+      (text, key) -> (byte) ((text + key) % 256);
+  private final static Operation decipherOp =
+      (text, key) -> (byte) ((text - key + 256) % 256);
+  private String key;
+
+  public VigenereCipher(String key) {
+    setKey(key);
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public byte[] encrypt(byte[] text) {
+    return processVigenere(text, encipherOp);
+  }
+
+  public byte[] decrypt(byte[] text) {
+    return processVigenere(text, decipherOp);
+  }
+
+  private byte[] processVigenere(byte[] text, Operation operation) {
+    byte[] bytes = new byte[text.length];
+
+    for (int i = 0, j = 0; i < text.length; ++i, j = (j + 1) % key.length()) {
+      byte c = text[i];
+      bytes[i] = operation.operate(c, key.charAt(j));
+    }
+
+    return bytes;
+  }
+
+  private interface Operation {
+    byte operate(byte text, char key);
+  }
+}
