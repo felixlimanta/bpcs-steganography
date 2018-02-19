02594e67d31af4423f830239d0f5f7f3518602e6
Felix Limanta <13515065@std.stei.itb.ac.id>
2018-02-19 13:53:34 +0700

    Added PSNR calculation

---
+++ src/main/java/PsnrCalculator.java
@@ -0,0 +1,21 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+public class PsnrCalculator {
+  public static double calculatePSNR(BufferedImage i1, BufferedImage i2) {
+    long s = 0;
+
+    byte[] data1 = ((DataBufferByte) i1.getRaster().getDataBuffer()).getData();
+    byte[] data2 = ((DataBufferByte) i2.getRaster().getDataBuffer()).getData();
+
+    if (data1.length != data2.length)
+      throw new IllegalArgumentException("Different image size");
+
+    for (int i = 0; i < data1.length; ++i) {
+      int d = (data1[i] & 0xff) - (data2[i] & 0xff);
+      s += (d * d);
+    }
+
+    return Math.sqrt(((double) s) / data1.length);
+  }
+}
